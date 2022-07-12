1475ce4a2d2f3ba38444996d0995c7c30806af1b
Taruun <phantmetic@gmail.com>
2022-07-12 22:26:13 +0530

    Updates

---
+++ precache-manifest.fe01313bffc891dd5c4c0aa484dd2e23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90864e6f05951b8c97badd333b472d35",
+    "url": "/Assignment/index.html"
+  },
+  {
+    "revision": "42682da5b14bb17a8d43",
+    "url": "/Assignment/static/css/main.77fb4cda.chunk.css"
+  },
+  {
+    "revision": "e6d8d3816acc93b53fb5",
+    "url": "/Assignment/static/js/2.5a2f2d94.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Assignment/static/js/2.5a2f2d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42682da5b14bb17a8d43",
+    "url": "/Assignment/static/js/main.18593520.chunk.js"
+  },
+  {
+    "revision": "93b31f88f235e4493bbd",
+    "url": "/Assignment/static/js/runtime-main.7d74acd1.js"
+  },
+  {
+    "revision": "900f5d2e49a0d3d85684fe83bdde3618",
+    "url": "/Assignment/static/media/logo.900f5d2e.svg"
+  }
+]);
